17d7a96abfd1e93425e8898d0e2bc6a2cdb30c74
Yixuan Wang <31319954+wyx951029@users.noreply.github.com>
2018-04-30 20:30:34 -0400

    Delete k_means_int.py

--- k_means_int.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def k_means_int(k,data,radom_state=42):     
-    X = data
-    rng = np.random.RandomState(radom_state)
-    i = rng.permutation(X.shape[0])[:k]
-    C = X[i]
-    return C
